57180c338911f9080ee7757bbed518032dde9dd0
jayapath1 <a1921796@adelaide.edu.au>
2025-06-20 03:41:26 +0000

    [automatic commit] 2025-06-20 03:41:26 UTC 139.218.236.184

--- part1/app.js
+++ part1/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
