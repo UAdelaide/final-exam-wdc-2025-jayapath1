4bc3aae1380219e824b37e92e6a0c36363828243
jayapath1 <a1921796@adelaide.edu.au>
2025-06-20 06:23:12 +0000

    [automatic commit] 2025-06-20 06:23:12 UTC 139.218.236.184

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // ✅ To handle form POST data (from login form)
+app.use(express.urlencoded({ extended: true })); // To handle form POST data (from login form)
 
 // ✅ Session middleware setup
 app.use(session({
